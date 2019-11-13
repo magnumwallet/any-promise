a153c471d9eef419babc6567e52b1f624fbe348e
Magnum Wallet <magnumwallet@gmail.com>
2019-11-13 20:55:57 +0300

    Update register-shim.js

--- register-shim.js
+++ register-shim.js
@@ -1,18 +1,18 @@
 "use strict";
-module.exports = require('./loader')(window, loadImplementation)
+module.exports = require('./loader')(global, loadImplementation)
 
 /**
- * Browser specific loadImplementation.  Always uses `window.Promise`
+ * Browser specific loadImplementation.  Always uses `global.Promise`
  *
  * To register a custom implementation, must register with `Promise` option.
  */
 function loadImplementation(){
-  if(typeof window.Promise === 'undefined'){
+  if(typeof global.Promise === 'undefined'){
     throw new Error("any-promise browser requires a polyfill or explicit registration"+
       " e.g: require('any-promise/register/bluebird')")
   }
   return {
-    Promise: window.Promise,
-    implementation: 'window.Promise'
+    Promise: global.Promise,
+    implementation: 'global.Promise'
   }
 }
